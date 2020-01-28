93dc128bd98b5deed3010a504d58d2a045a16a3b
Ivan Feng <feng.ivan.1235@gmail.com>
2020-01-27 22:22:23 -0500

    edit

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 app.get('/api/', async (request, response) => {
-    const api_key = '3d0cf5aa23b128c122fc7588b928d1b'
+
+    // const api_key = '3d0cf5aa23b128c122fc7588b928d1b';
+    const api_key = process.env.API_KEY;
     const api_url = `http://api.openweathermap.org/data/2.5/forecast?q=ottawa,CA&mode=json&appid=${api_key}`
     const rawResp = await fetch(api_url)
     const data = await rawResp.json()
