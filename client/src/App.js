d7ae99141e93afa96dcdbc4bb3730ff99ab2e333
Ivan Feng <feng.ivan.1235@gmail.com>
2020-02-13 16:48:30 -0500

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,9 @@ export default function App() {
 
     const fetchData = async () => {
 
-      const response = await fetch(`/api/`)
+      const response = await fetch(`/api/${city}`)
+      // for some reason, fetching from heroku is weird so I add this to make sure it fetchs the weather data and then parse it into json
+
       var data = await response.text()
       data = data.replace(/\\/g, '');
       if (data[0] == "\"" && data[data.length - 1] == "\"") {
