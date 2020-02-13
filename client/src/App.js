e03c20c7d7d795520493127a91b2baf966fc539d
Ivan Feng <feng.ivan.1235@gmail.com>
2020-02-13 17:38:34 -0500

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ export default function App() {
 
     const fetchData = async () => {
 
-      const response = await fetch(`/api/`)
+      const response = await fetch(`/api/${city}`)
       console.log(response)
       // for some reason, fetching from heroku is weird so I add this to make sure it fetchs the weather data and then parse it into json
 
--- index.js
+++ index.js
@@ -25,10 +25,11 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
-app.get('/api/', async (request, response) => {
+app.get('/api/:id', async (request, response) => {
     const api_key = '3d0cf5aa23b128c122fc7588b928d1b5'
-    // const city = request.params.city
-    const city = 'toronto'
+    const city = request.params.id
+    console.log(city)
+    // const city = 'toronto'
     const api_url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},CA&mode=json&appid=${api_key}`
     const rawResp = await fetch(api_url)
     const data = await rawResp.json()
