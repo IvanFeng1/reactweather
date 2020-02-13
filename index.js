7c96bd0fea20cb9564cf20d3ea9dc6fcfbf0cc6c
Ivan Feng <feng.ivan.1235@gmail.com>
2020-02-13 17:03:20 -0500

    edit

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 app.get('/api/:city', async (request, response) => {
     const api_key = '3d0cf5aa23b128c122fc7588b928d1b5'
-    const city = request.params.city
+    // const city = request.params.city
+    const city = 'toronto'
     const api_url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},CA&mode=json&appid=${api_key}`
     const rawResp = await fetch(api_url)
     const data = await rawResp.json()
